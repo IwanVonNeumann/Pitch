db6dfb8fe6268f57a725aeecb75783c15be4fce4
IwanVonNeumann <grabageheap@inbox.lv>
2017-08-09 08:49:49 +0300

    jQuery-based menu introduced

---
+++ js/Menu.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Iwan on 09.08.2017.
+ */
+
+$(document).ready(function () {
+
+    $(".js-select-ex-melody").addClass("selected");
+    $(".js-select-ins-piano").addClass("selected");
+
+
+
+});
