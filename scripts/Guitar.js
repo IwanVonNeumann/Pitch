6d735d462de1a2b1ff3e961200a7e1ec6320185d
IwanVonNeumann <grabageheap@inbox.lv>
2017-08-27 18:53:22 +0300

    Guitar & Violin classes implemented

--- scripts/Guitar.js
+++ scripts/Guitar.js
@@ -2,6 +2,24 @@
  * Created by Iwan on 27.08.2017.
  */
 
-define("Guitar", function () {
-    return {};
-});
+define("Guitar",
+    ["AbstractInstrument", "Instrument", "SoundManager"],
+        function (AbstractInstrument, Instrument, SoundManager) {
+
+            var Guitar = function () {
+                AbstractInstrument.apply(this, arguments);
+                this.notes = SoundManager.loadSounds(Instrument.GUITAR);
+            };
+
+            Guitar.prototype.__proto__ = AbstractInstrument.prototype;
+
+            var singletonGuitar;
+
+            return {
+                get: function () {
+                    singletonGuitar = singletonGuitar || new Guitar();
+                    return singletonGuitar;
+                }
+            };
+        }
+);
--- scripts/Violin.js
+++ scripts/Violin.js
@@ -2,6 +2,24 @@
  * Created by Iwan on 27.08.2017.
  */
 
-define("Violin", function () {
-    return {};
-});
+define("Violin",
+    ["AbstractInstrument", "Instrument", "SoundManager"],
+    function (AbstractInstrument, Instrument, SoundManager) {
+
+        var Violin = function () {
+            AbstractInstrument.apply(this, arguments);
+            this.notes = SoundManager.loadSounds(Instrument.VIOLIN);
+        };
+
+        Violin.prototype.__proto__ = AbstractInstrument.prototype;
+
+        var singletonViolin;
+
+        return {
+            get: function () {
+                singletonViolin = singletonViolin || new Violin();
+                return singletonViolin;
+            }
+        };
+    }
+);
