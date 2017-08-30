f25a65127091edbc4c0e2dee6c9a5fd229efdde8
IwanVonNeumann <grabageheap@inbox.lv>
2017-08-30 13:31:02 +0300

    ResourceLoader cleaned up

--- scripts/ResourceLoader.js
+++ scripts/ResourceLoader.js
@@ -1,65 +1,20 @@
 "use strict";
 
-define("ResourceLoader", ["EventBus"], function (EventBus) {
+// TODO remove this
+define("ResourceLoader", function () {
 
     return {
-        // sounds: [],
-        // images: [],
         loadedCount: 0,
 
-        // TODO create constructor
         init: function () {
-            // this.sounds = [];
-            // this.images = [];
             this.loadedCount = 0;
-
-            EventBus.bind("resource:load", function () {
-
-            }, this);
-        },
-/*
-        registerImage: function (path) {
-            this.images.push(path);
         },
 
-        registerSound: function (path) {
-            this.sounds.push(path);
-        },
-*/
         loadAll: function (displayProgress, allResourcesLoaded) {
             this.displayProgress = displayProgress;
             this.allResourcesLoaded = allResourcesLoaded;
-            // this.loadImages();
-            // this.loadSounds();
-        },
-/*
-        loadImages: function () {
-            function imageLoadingAborted() {
-                console.log("resource abortion");
-            }
-
-            function imageLoadingError() {
-                console.log("resource error");
-            }
-
-            for (var i = 0; i < this.images.length; ++i) {
-                var path = this.images[i];
-                this.images[i] = new Image();
-                this.images[i].onload = this.resourceLoaded.bind(this);
-                this.images[i].onerror = imageLoadingAborted;
-                this.images[i].onabort = imageLoadingError;
-                this.images[i].src = path;
-            }
         },
 
-        loadSounds: function () {
-            for (var i = 0; i < this.sounds.length; ++i) {
-                var path = this.sounds[i];
-                this.sounds[i] = new Audio(path);
-                this.sounds[i].addEventListener('canplaythrough', this.resourceLoaded());
-            }
-        },
-*/
         // never called now
         resourceLoaded: function () {
             this.loadedCount++;
